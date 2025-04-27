6b28deac8ccf04abe9d8da362f6f3a92d535f667
savyu <33470153+savyu@users.noreply.github.com>
2025-04-27 13:58:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/conference_event_planner",
+    base: "/github_pages_test_repo",
   plugins: [react()],
 })
